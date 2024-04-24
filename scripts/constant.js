1e27ba83eb9ceff3b7a73ed7224e0a890006c7ca
unknown <mr.krabopl12@gmail.com>
2024-04-24 20:48:04 +0400

    Change port

--- scripts/constant.js
+++ scripts/constant.js
@@ -3,7 +3,7 @@ const path = require('path')
 const PROJECT_PATH = path.resolve(__dirname, '../')   // Project root path
 
 const SERVER_HOST = '127.0.0.1'
-const SERVER_PORT = 3000
+const SERVER_PORT = 5000
 
 module.exports= {
     PROJECT_PATH,
